fbb5ec9785d215135e8f35c61f024616d444e472
JeanBaptisteWATENBERG <jean-baptiste.watenberg@isep.fr>
2017-12-03 21:43:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var input = [];
+
+readline_object.on("line", (value) => { //Read input values
+	input.push(value);
+})
+//Call ContestResponse when all inputs are red
+readline_object.on("close", ContestResponse); 
+
+String.prototype.i = function() {
+  retrun parseInt(this, 10);
+}
+
+String.prototype.f = function() {
+  return parseFloat(this)
+}
+
+function pr(i) {
+  if (Array.isArray(i)) {
+    LocalPrintArray( i ); 
+  } else {
+    LocalPrint( i ); 
+  }
+}
+
+function ContestResponse(){
+  const n = input.shift().i()
+  //const m = input.shift()
+  //const last = input.pop()
+  //input.
+  
+}
+
+
